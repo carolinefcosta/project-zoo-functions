839009b07b2c763568207e81ed4a927a3abb1d61
Caroline <carolinefernandesc@hotmail.com>
2022-12-08 18:57:39 -0300

    linter corrigindo

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,6 +1,8 @@
-const { species } = require('../data/zoo_data');
+const data = require('../data/zoo_data');
 
 const getAnimalsOlderThan = (animal, age) => {
 };
-console.log(getAnimalsOlderThan('otters', 7));
+
+// console.log(getAnimalsOlderThan('otters', 7));
+
 module.exports = getAnimalsOlderThan;
