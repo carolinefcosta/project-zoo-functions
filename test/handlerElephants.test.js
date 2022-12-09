a8598711237fa9ce3844e219986816954c36b375
Caroline <carolinefernandesc@hotmail.com>
2022-12-09 10:01:45 -0300

    requisito 07

--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -10,4 +10,16 @@ describe('Testes da função HandlerElephants', () => {
   it('Verificando retorno da função com parâmetro average', () => {
     expect(handlerElephants('averageAge')).toBeCloseTo(10.5);
   });
+  it('Verificando retorno da função com parâmetro location', () => {
+    expect(handlerElephants('location')).toBe('NW');
+  });
+  it('Verificando retorno da função com parâmetro popularity', () => {
+    expect(handlerElephants('popularity')).toBeGreaterThanOrEqual(5);
+  });
+  it('Verificando retorno da função com parâmetro availability', () => {
+    expect(handlerElephants('availability')).toEqual(['Friday', 'Saturday', 'Sunday', 'Tuesday']);
+  });
+  it('Verificando retorno da função sem parâmetros', () => {
+    expect(handlerElephants()).toBe(undefined);
+  });
 });
