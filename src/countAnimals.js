e0067983cfdf4e426b3e62f7f856d1bfb830f8d1
Caroline <carolinefernandesc@hotmail.com>
2022-12-09 09:34:18 -0300

    requisito 06

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,19 @@
-const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
 
 const countAnimals = (animal) => {
-  // seu código aqui
+  if (animal === undefined) {
+    return species.reduce((acc, curr) => {
+      acc[curr.name] = curr.residents.length;
+      return acc;
+    }, {});
+  }
+  const { residents } = species.find(({ name }) => name === animal.specie);
+  if (animal.sex) {
+    return residents.filter(({ sex }) => sex === animal.sex).length;
+  }
+  return residents.length;
 };
+
+console.log(countAnimals({ species: 'giraffes' }));
 
 module.exports = countAnimals;
--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -1,3 +1,13 @@
 const handlerElephants = require('../src/handlerElephants');
 
-describe('Testes da função HandlerElephants', () => {});
+describe('Testes da função HandlerElephants', () => {
+  it('Verifica quantidade de elefantes', () => {
+    expect(handlerElephants('count')).toBe(4);
+  });
+  it('Verificando retorno da função com parâmetro names', () => {
+    expect(handlerElephants('names')).toContain('Jefferson');
+  });
+  it('Verificando retorno da função com parâmetro average', () => {
+    expect(handlerElephants('averageAge')).toBeCloseTo(10.5);
+  });
+});
