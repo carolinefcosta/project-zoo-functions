c7fd22571aa1b3e5977a671fecb980a07b5576a4
Caroline <carolinefernandesc@hotmail.com>
2022-12-10 16:39:16 -0300

    refazendo lógica desafio 08

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,22 +1,23 @@
 const { prices } = require('../data/zoo_data');
 
 const countEntrants = (entrants) => {
-  const child = entrants.filter((entrant) => entrant.age < 18).length;
-
-  const adult = entrants.filter((entrant) => entrant.age >= 18 && entrant.age < 50).length;
-
-  const senior = entrants.filter((entrant) => entrant.age >= 50).length;
-
-  return { child, adult, senior };
+  const childs = entrants.filter((entrant) => entrant.age < 18).length;
+  const adults = entrants.filter((entrant) => entrant.age >= 18 && entrant.age < 50).length;
+  const seniors = entrants.filter((entrant) => entrant.age >= 50).length;
+  return {
+    child: childs,
+    adult: adults,
+    senior: seniors,
+  };
 };
 
 const calculateEntry = (entrants) => {
-  if (entrants === undefined || entrants.length === {}) {
+  if (entrants === undefined || !entrants.length) {
     return 0;
   }
   const { child, adult, senior } = countEntrants(entrants);
   const result = prices.child * child + prices.adult * adult + prices.senior * senior;
-  return result.toFixed(2);
+  return result;
 };
 
 module.exports = { calculateEntry, countEntrants };
