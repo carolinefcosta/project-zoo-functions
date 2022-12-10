c5181c039743b00453c176091957a5c1910e1592
Caroline <carolinefernandesc@hotmail.com>
2022-12-10 09:34:26 -0300

    requisito 08

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,22 @@
-const data = require('../data/zoo_data');
+const { prices } = require('../data/zoo_data');
 
 const countEntrants = (entrants) => {
-  // seu código aqui
+  const child = entrants.filter((entrant) => entrant.age < 18).length;
+
+  const adult = entrants.filter((entrant) => entrant.age >= 18 && entrant.age < 50).length;
+
+  const senior = entrants.filter((entrant) => entrant.age >= 50).length;
+
+  return { child, adult, senior };
 };
 
 const calculateEntry = (entrants) => {
-  // seu código aqui
+  if (entrants === undefined || entrants.length === {}) {
+    return 0;
+  }
+  const { child, adult, senior } = countEntrants(entrants);
+  const result = prices.child * child + prices.adult * adult + prices.senior * senior;
+  return result.toFixed(2);
 };
 
 module.exports = { calculateEntry, countEntrants };
--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -3,8 +3,8 @@ const { employees, species } = require('../data/zoo_data');
 const getOldestFromFirstSpecies = (id) => {
   const person = employees.find((employee) => employee.id === id).responsibleFor[0];
   const animal = species.find((specie) => specie.id === person).residents;
-  const resultOder = animal.sort((animal1, animal2) => animal2.age - animal1.age)[0];
-  return Object.values(resultOder);
+  const result = animal.sort((animal1, animal2) => animal2.age - animal1.age)[0];
+  return Object.values(result);
 };
 
 module.exports = getOldestFromFirstSpecies;
