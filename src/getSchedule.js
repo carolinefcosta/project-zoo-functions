d3f5a8da506635196d80a1f20f77ffe1565efa2e
Caroline <carolinefernandesc@hotmail.com>
2022-12-11 17:22:15 -0300

    corrigindo linter

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,11 +1,10 @@
 const { species } = require('../data/zoo_data');
 
 const getSchedule = (scheduleTarget) => {
-  const diasSemana = species.filter((specie) => {
-    return specie.name === scheduleTarget;
-  });
+  const diasSemana = species.filter((specie) => specie.name === scheduleTarget);
   return diasSemana[0].availability;
 };
 
 console.log(getSchedule('tigers'));
+
 module.exports = getSchedule;
