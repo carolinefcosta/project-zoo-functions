6bbdbf7c9ddadc0aa29d98933c0ce29603257755
Caroline <carolinefernandesc@hotmail.com>
2022-12-14 09:57:35 -0300

    requisito 16

--- test/getOpeningHours.test.js
+++ test/getOpeningHours.test.js
@@ -12,25 +12,31 @@ describe('Testes da função getOpeningHours', () => {
       Monday: { open: 0, close: 0 },
     });
   });
-  it('Verifica a função com os parâmetros "Monday e 09:00-AM""', () => {
+  it('Verifica a função com os parâmetros ("Monday e 09:00-AM")', () => {
     expect(getOpeningHours('Monday', '09:00-AM')).toBe('The zoo is closed');
   });
-  it('Verifica a função com os parâmetros "Tuesday e 09:00-AM"', () => {
+  it('Verifica a função com os parâmetros ("Tuesday e 09:00-AM")', () => {
     expect(getOpeningHours('Tuesday', '09:00-AM')).toBe('The zoo is open');
   });
-  it('Verifica a função com os parâmetros "Wednesday e 09:00-PM"', () => {
+  it('Verifica a função com os parâmetros ("Wednesday e 09:00-PM")', () => {
     expect(getOpeningHours('Wednesday', '09:00-PM')).toBe('The zoo is closed');
   });
   it('Verificando a função com exceções', () => {
     expect(() => getOpeningHours('Thu', '09:00-AM')).toThrowError(new Error('The day must be valid. Example: Monday'));
   });
-  it('Verifica a função com exceções com toThrowError', () => {
-    expect(() => getOpeningHours('Friday', '09:00-ZM')).toThrowError(new Error('The abbreviation must be \'AM\' or \'PM\''));
+  it('Verifica a função com exceções toThrowError', () => {
+    expect(() => getOpeningHours('Friday', '09:00-WM')).toThrowError(new Error('The abbreviation must be \'AM\' or \'PM\''));
   });
-  it('Verifica a função com exceções', () => {
+  it('Verifica a função com exceções de Error', () => {
     expect(() => getOpeningHours('Saturday', 'C9:00-AM')).toThrowError(new Error('The hour should represent a number'));
   });
-  it('Verifica a função com exceções', () => {
+  it('Verifica a função com exceções new Error', () => {
     expect(() => getOpeningHours('Sunday', '09:c0-AM')).toThrowError(new Error('The minutes should represent a number'));
+  });
+  it('Verifica a função com Error, passando parâmetros ("Monday", "15:00-AM")', () => {
+    expect(() => getOpeningHours('Monday', '15:00-AM')).toThrowError(new Error('The hour must be between 0 and 12'));
+  });
+  it('Verifica a função com Error, passando parâmetros ("Tuesday", "09:61-AM")', () => {
+    expect(() => getOpeningHours('Tuesday', '09:61-AM')).toThrowError(new Error('The minutes must be between 0 and 59'));
   });
 });
